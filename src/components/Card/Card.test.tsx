6e4c3ca9b585f4a0579cef0f8e69bd8fc75ae173
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-03-21 15:59:39 +0500

    fix: fix routing error

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,20 +1,40 @@
 import { render, screen } from '@testing-library/react';
 import Card, { ICard } from './Card';
 
-const testCard: ICard = {
-  imgUrl: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/x3PIk93PTbxT88ohfeb26L1VpZw.jpg',
-  name: 'Prizefighter: The Life of Jem Belcher',
-  date: 'Jun 30, 2022',
-  score: '71%',
-};
+const testCards: Array<ICard> = [
+  {
+    imgUrl: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/kuf6dutpsT0vSVehic3EZIqkOBt.jpg',
+    name: 'Puss in Boots: The Last Wish',
+    date: 'Dec 07, 2022',
+    score: '84%',
+  },
+  {
+    imgUrl: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/x3PIk93PTbxT88ohfeb26L1VpZw.jpg',
+    name: 'Prizefighter: The Life of Jem Belcher',
+    date: 'Jun 30, 2022',
+    score: '71%',
+  },
+];
+
 describe('Card component', () => {
   test('it renders', () => {
-    render(<Card {...testCard} />);
+    render(<Card {...testCards[0]} />);
   });
-  it('should props it into the apropriate text', () => {
-    render(<Card {...testCard} />);
-    expect(screen.getByText(testCard.name)).toBeInTheDocument();
-    expect(screen.getByText(testCard.date)).toBeInTheDocument();
-    expect(screen.getByText(`Score: ${testCard.score}`)).toBeInTheDocument();
+  it('renders props it into the apropriate text', () => {
+    render(<Card {...testCards[0]} />);
+    expect(screen.getByText(testCards[0].name)).toBeInTheDocument();
+    expect(screen.getByText(testCards[0].date)).toBeInTheDocument();
+    expect(screen.getByText(`Score: ${testCards[0].score}`)).toBeInTheDocument();
+  });
+  it('renders a list of product cards', () => {
+    render(
+      <>
+        {testCards.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </>
+    );
+    const scoreElements = screen.getAllByText(/Score:/i);
+    expect(scoreElements.length).toBe(testCards.length);
   });
 });
--- src/components/Header/Header.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Hearder from './Hearder';
-
-describe('About', () => {
-  test('it renders', () => {
-    render(<Hearder />);
-  });
-});
