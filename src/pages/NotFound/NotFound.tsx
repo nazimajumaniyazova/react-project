d3173becba5f8b781b4ddf59037865585a091e29
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-03-29 00:15:26 +0500

    refactor: change NotFound  component to hooks

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
 import './NotFound.css';
-export default class NotFound extends Component {
-  render() {
-    return (
-      <div className="centralize not-found">
-        <h1>Oops!</h1>
-        <p>Page Not Found</p>
-      </div>
-    );
-  }
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="centralize not-found">
+      <h1>Oops!</h1>
+      <p>Page Not Found</p>
+    </div>
+  );
 }
+
+export default NotFound;
