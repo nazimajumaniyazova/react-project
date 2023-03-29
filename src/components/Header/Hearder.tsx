9069ccf669ca49951194f0033aab4b54254a3759
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-03-29 05:11:01 +0500

    refactor: change Header from class components to function

--- src/components/Header/Hearder.tsx
+++ src/components/Header/Hearder.tsx
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
-export default class Hearder extends Component {
-  render() {
-    return (
-      <header className="header">
-        <div className="container centralize header__wrapper">
-          <a className="logo">Logo</a>
-          <ul className="menu">
-            <li className="menu__item">
-              <NavLink to={`/`} className="menu__link">
-                Home
-              </NavLink>
-            </li>
-            <li className="menu__item">
-              <NavLink to={`about-us/`} className="menu__link">
-                About Us
-              </NavLink>
-            </li>
-            <li className="menu__item">
-              <NavLink to={`forms/`} className="menu__link">
-                Forms
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </header>
-    );
-  }
+
+function Hearder() {
+  return (
+    <header className="header">
+      <div className="container centralize header__wrapper">
+        <a className="logo">Logo</a>
+        <ul className="menu">
+          <li className="menu__item">
+            <NavLink to={`/`} className="menu__link">
+              Home
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink to={`about-us/`} className="menu__link">
+              About Us
+            </NavLink>
+          </li>
+          <li className="menu__item">
+            <NavLink to={`forms/`} className="menu__link">
+              Forms
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
+
+export default Hearder;
