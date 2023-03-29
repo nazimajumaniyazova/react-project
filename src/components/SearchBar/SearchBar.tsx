fd80521cc6557d798803edd9c2262ec2c3f5642a
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-03-29 21:07:30 +0500

    refactor: change SearchBar component from class to func

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,49 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
-interface IProps {
-  smth?: string;
+function SearchBar() {
+  const [searchValue, setSearchValue] = useState(() => {
+    const saved = localStorage.getItem('searchV');
+    return saved || '';
+  });
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    localStorage.setItem('searchV', event.target.value);
+    setSearchValue(event.target.value);
+  };
+  return (
+    <div className="search-bar-section">
+      <div className="container centralize search-bar__wrapper">
+        <p className="search-bar__title">Go ahead, hover over search</p>
+        <form className="search-bar">
+          <input
+            type="text"
+            className="search-bar__input"
+            placeholder="Search"
+            onInput={onInputChange}
+            value={searchValue}
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
 
-interface IState {
-  value: string;
-}
-export default class SearchBar extends Component<IProps, IState> {
-  valueLocal: string | null;
-  constructor(props: IProps) {
-    super(props);
-    this.valueLocal = localStorage.getItem('inputValue');
-    if (!this.valueLocal) {
-      this.valueLocal = ' ';
-    }
-    console.log(this.valueLocal);
-    this.state = {
-      value: this.valueLocal,
-    };
-  }
-  onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    this.setState({ value: event.target.value });
-  };
-  componentWillUnmount() {
-    localStorage.setItem('inputValue', this.state.value);
-  }
-  render() {
-    return (
-      <div className="search-bar-section">
-        <div className="container centralize search-bar__wrapper">
-          <p className="search-bar__title">Go ahead, hover over search</p>
-          <form className="search-bar">
-            <input
-              type="text"
-              className="search-bar__input"
-              placeholder="Search"
-              onInput={this.onInputChange}
-              value={this.state.value}
-            />
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
+export default SearchBar;
