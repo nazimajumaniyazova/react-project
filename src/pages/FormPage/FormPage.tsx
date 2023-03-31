e616060d51dc28c0fd89518a45ddef059698086b
nazimajumaniyazova <nazima.jumaniyazova.95@gmail.com>
2023-03-31 13:45:31 +0500

    feat: add form validation

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,5 +1,6 @@
 import './FormPage.css';
 import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 const countries = [
   'USA',
   'UK',
@@ -21,50 +22,112 @@ interface Idata {
   image: string;
 }
 function FormPage() {
-  const { register, handleSubmit } = useForm<Idata>();
-  const onSubmit = (data: Idata) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Idata>();
+  const onSubmit = (data: Idata) => {
+    console.log(data);
+  };
   return (
     <div className="form-page">
       <h2 className="form-title">Save your favourite movie</h2>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="set">
           <div className="name">
-            <label htmlFor="m-name">Name</label>
-            <input type="text" id="m-name" {...register('name')} />
+            <label htmlFor="m-name" className="required">
+              Name
+            </label>
+            <input
+              type="text"
+              id="m-name"
+              {...register('name', {
+                required: 'This field is required.',
+                minLength: {
+                  value: 3,
+                  message: 'This input should contain at least 3 letters',
+                },
+              })}
+              className={errors.name ? 'error-validate' : ''}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="name"
+              render={({ message }) => <p className="error-message">{message}</p>}
+            />
           </div>
           <div className="poster">
-            <input
-              type="file"
-              accept="image/*"
-              id="poster-upload"
-              className="poster-upload-input"
-              {...register('image')}
-            />
-            <label htmlFor="poster-upload" className="poster-upload-label">
-              <span className="poster-upload-icon">
-                <i className="fas fa-camera-retro"></i>
-              </span>
-              Upload a poster
-            </label>
+            <div>
+              <input
+                type="file"
+                accept="image/*"
+                id="poster-upload"
+                className="poster-upload-input"
+                {...register('image', { required: 'This field is required.' })}
+              />
+              <label htmlFor="poster-upload" className="poster-upload-label required">
+                <span className="poster-upload-icon">
+                  <i className="fas fa-camera-retro"></i>
+                </span>
+                Upload a poster
+              </label>
+            </div>
+            <div>
+              <ErrorMessage
+                errors={errors}
+                name="image"
+                render={({ message }) => <p className="error-message">{message}</p>}
+              />
+            </div>
           </div>
         </div>
         <div className="set">
           <div className="date">
-            <label htmlFor="m-date">Premiere date</label>
-            <input id="m-date" placeholder="MM/DD/YYYY" type="date" {...register('date')} />
+            <label htmlFor="m-date" className="required">
+              Premiere date
+            </label>
+            <input
+              id="m-date"
+              placeholder="MM/DD/YYYY"
+              type="date"
+              {...register('date', { required: 'This field is required.' })}
+            />
+            <ErrorMessage
+              errors={errors}
+              name="date"
+              render={({ message }) => <p className="error-message">{message}</p>}
+            />
           </div>
           <div className="status">
-            <label htmlFor="status-released">Status</label>
+            <label htmlFor="status-released" className="required">
+              Status
+            </label>
             <div className="radio-container">
-              <input id="status-released" type="radio" value="Released" {...register('status')} />
+              <input
+                id="status-released"
+                type="radio"
+                value="Released"
+                {...register('status', { required: 'This field is required.' })}
+              />
               <label htmlFor="status-released">Released</label>
-              <input id="status-upcoming" type="radio" value="Upcoming" {...register('status')} />
+              <input
+                id="status-upcoming"
+                type="radio"
+                value="Upcoming"
+                {...register('status', { required: 'This field is required.' })}
+              />
               <label htmlFor="status-upcoming">Upcoming</label>
             </div>
+            <ErrorMessage
+              errors={errors}
+              name="status"
+              render={({ message }) => <p className="error-message">{message}</p>}
+            />
           </div>
         </div>
         <div className="set set-checkbox">
-          <label className="label">Genres</label>
+          <label className="label required">Genres</label>
           <ul className="checkboxes">
             <li className="checkbox">
               <input
@@ -72,7 +135,7 @@ function FormPage() {
                 id="choice-0"
                 type="checkbox"
                 value="comedy"
-                {...register('genres')}
+                {...register('genres', { required: 'This field is required.' })}
               />
               <label className="checkbox-label" htmlFor="choice-0">
                 Comedy
@@ -84,7 +147,7 @@ function FormPage() {
                 id="choice-1"
                 type="checkbox"
                 value="drama"
-                {...register('genres')}
+                {...register('genres', { required: 'This field is required.' })}
               />
               <label className="checkbox-label" htmlFor="choice-1">
                 Drama
@@ -96,7 +159,7 @@ function FormPage() {
                 id="choice-2"
                 type="checkbox"
                 value="fantasy"
-                {...register('genres')}
+                {...register('genres', { required: 'This field is required.' })}
               />
               <label className="checkbox-label" htmlFor="choice-2">
                 Fantasy
@@ -108,7 +171,7 @@ function FormPage() {
                 id="choice-3"
                 type="checkbox"
                 value="horror"
-                {...register('genres')}
+                {...register('genres', { required: 'This field is required.' })}
               />
               <label className="checkbox-label" htmlFor="choice-3">
                 Horror
@@ -120,7 +183,7 @@ function FormPage() {
                 id="choice-4"
                 type="checkbox"
                 value="romance"
-                {...register('genres')}
+                {...register('genres', { required: 'This field is required.' })}
               />
               <label className="checkbox-label" htmlFor="choice-4">
                 Romance
@@ -132,24 +195,43 @@ function FormPage() {
                 id="choice-5"
                 type="checkbox"
                 value="thriller"
-                {...register('genres')}
+                {...register('genres', { required: 'This field is required.' })}
               />
               <label className="checkbox-label" htmlFor="choice-5">
                 Thriller
               </label>
             </li>
           </ul>
+          <ErrorMessage
+            errors={errors}
+            name="genres"
+            render={({ message }) => <p className="error-message">{message}</p>}
+          />
         </div>
         <div className="set">
           <div className="countries">
-            <label htmlFor="country">Country</label>
-            <select id="country" {...register('country')}>
+            <label htmlFor="country" className="required">
+              Country
+            </label>
+            <select
+              id="country"
+              defaultValue={''}
+              {...register('country', { required: 'This field is required.' })}
+            >
+              <option disabled value="">
+                Choose here
+              </option>
               {countries.sort().map((item, index) => (
                 <option value={item} key={index}>
                   {item}
                 </option>
               ))}
             </select>
+            <ErrorMessage
+              errors={errors}
+              name="country"
+              render={({ message }) => <p className="error-message">{message}</p>}
+            />
           </div>
         </div>
         <div className="set">
